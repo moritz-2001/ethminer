8c889bf326f08a8e59a547a34a5221e964b9070d
Moritz <moritz>
2024-09-21 18:05:16 +0200

    Comment out

--- ethminer/libethash-sycl/fnv.dp.hpp
+++ ethminer/libethash-sycl/fnv.dp.hpp
@@ -28,7 +28,7 @@ DEV_INLINE sycl::uint4 fnv4_p(bool allow, const sycl::stream &out, sycl::uint4 a
 {
     sycl::uint4 c;
     if (allow) {
-       out << "fnv4 " << a << " " << b << "\n";
+       //out << "fnv4 " << a << " " << b << "\n";
     }
 
     c.x() = a.x() * FNV_PRIME ^ b.x();
